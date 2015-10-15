3822b2d13061fc707105e881df8fb3c4e452883e
Svirchevsky Nicolas <svirchevsky@a11p11.fil.univ-lille1.fr>
2015-10-15 14:23:05 +0200

    init gradle

---
+++ src/main/java/lille1/piscine/Main.java
@@ -0,0 +1,7 @@
+package lille1.Piscine;
+
+public class Main {
+    public static void main(String argv[]) {
+        System.out.println("salut");
+    }
+}
---
+++ src/test/java/lille1/piscine/MainTest.java
@@ -0,0 +1,13 @@
+package lille1.Piscine;
+
+
+/**
+ * Created by svirchevsky on 15/10/15.
+ */
+public class MainTest {
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
