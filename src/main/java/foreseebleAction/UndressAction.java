5b0e133f0bdecb0758e5e3977a80bafe4e828ae1
Wattier Julian <wattier@a11p16.fil.univ-lille1.fr>
2015-10-22 17:04:53 +0200

    adding undressAction subclass of foreseeble

---
+++ src/main/java/foreseebleAction/UndressAction.java
@@ -0,0 +1,10 @@
+package foreseebleAction;
+
+/**
+ * Created by wattier on 22/10/15.
+ */
+public class UndressAction extends ForeseebleAction {
+    public UndressAction(int timeToEnd) {
+        super(timeToEnd);
+    }
+}
