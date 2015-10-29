0c636ba9f323a80e00715681078247628f557f0d
wattie-j <wattier.julian@gmail.com>
2015-10-29 13:44:49 +0100

    adding SwimAction class

---
+++ src/main/java/foreseebleAction/SwimAction.java
@@ -0,0 +1,10 @@
+package foreseebleAction;
+
+import action.foreseebleAction.ForeseebleAction;
+
+/**
+ * Created by julian on 29/10/15.
+ */
+public class SwimAction extends ForeseebleAction {
+    public SwimAction(int timeToEnd) {super(timeToEnd);}
+}
